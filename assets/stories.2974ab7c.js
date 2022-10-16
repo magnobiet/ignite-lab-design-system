9c75def75bb30e9b40fc4c33c253c351a29e6e67
magnobiet <magnobiet@users.noreply.github.com>
2022-10-16 22:33:06 +0000

    deploy: fef57bbeccbcab662f811cffef99f1447be905f6

--- assets/stories.2974ab7c.js
+++
@@ -1,2 +0,0 @@
-import{L as o}from"./index.0a35d1ad.js";import"./jsx-runtime.fd102a39.js";import"./index.38dba348.js";import"./iframe.6e41682d.js";const r={title:"Components / Logo",component:o},s={};export{s as Default,r as default};
-//# sourceMappingURL=stories.2974ab7c.js.map
